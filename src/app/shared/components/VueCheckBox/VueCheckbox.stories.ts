621691a45961dc4a798bba6ee32c525690ea2b2a
Haruto0307 <skybolt0307@gmail.com>
2018-05-12 16:30:37 +0200

    feat(components): add example for form validation (#126)

--- src/app/shared/components/VueCheckBox/VueCheckbox.stories.ts
+++ src/app/shared/components/VueCheckBox/VueCheckbox.stories.ts
@@ -1,39 +1,39 @@
 import { storiesOf } from '@storybook/vue';
 import VueInfoAddon  from 'storybook-addon-vue-info';
-import VueCheckBox   from './VueCheckBox.vue';
+import VueCheckbox   from './VueCheckbox.vue';
 import { action }    from '@storybook/addon-actions';
 
-const story = (storiesOf('VueCheckBox', module) as any);
+const story = (storiesOf('VueCheckbox', module) as any);
 
 story.addDecorator(VueInfoAddon);
 
 story.add('Checkbox', () => ({
-  components: { VueCheckBox },
-  template:   `<vue-check-box label="checkbox" name="checkbox" id="checkbox" @click="action" />`,
+  components: { VueCheckbox },
+  template:   `<vue-checkbox label="checkbox" name="checkbox" id="checkbox" @click="action" />`,
   methods:    {
     action: action('@onClick'),
   },
 }));
 
 story.add('Checkbox Disabled', () => ({
-  components: { VueCheckBox },
-  template:   `<vue-check-box label="checkbox" name="checkbox" id="checkbox" disabled @click="action" />`,
+  components: { VueCheckbox },
+  template:   `<vue-checkbox label="checkbox" name="checkbox" id="checkbox" disabled @click="action" />`,
   methods:    {
     action: action('@onClick'),
   },
 }));
 
 story.add('Radio Button', () => ({
-  components: { VueCheckBox },
-  template:   `<vue-check-box label="radio button" name="radio" id="radio" radio @click="action" />`,
+  components: { VueCheckbox },
+  template:   `<vue-checkbox label="radio button" name="radio" id="radio" radio @click="action" />`,
   methods:    {
     action: action('@onClick'),
   },
 }));
 
 story.add('Radio Button disabled', () => ({
-  components: { VueCheckBox },
-  template:   `<vue-check-box label="radio button" name="radio" id="radio" radio disabled @click="action" />`,
+  components: { VueCheckbox },
+  template:   `<vue-checkbox label="radio button" name="radio" id="radio" radio disabled @click="action" />`,
   methods:    {
     action: action('@onClick'),
   },
